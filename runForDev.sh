3d0f4776754d3577d0f2d5f7a4d14294797f6233
Shilo B <shiloworks@gmail.com>
2017-05-15 10:48:11 +1000

    Added grunt-cli installation on initial development set up.

--- runForDev.sh
+++ runForDev.sh
@@ -7,7 +7,7 @@ do
         docker run -it --rm -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal && npm install sails-hook-autoreload && npm install -g typings && npm install"
     fi
     if [ $var = "compile" ]; then
-      docker run -it --rm -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal && npm install grunt -g && grunt --gruntfile Gruntfile-ts-compile.js"
+      docker run -it --rm -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal && npm install grunt-cli -g && grunt --gruntfile Gruntfile-ts-compile.js"
     fi
 done
 
