1f0620eeceb9536cbedf6eb535078789e67c22d4
Shilo B <shiloworks@gmail.com>
2017-05-17 16:53:37 +1000

    Moved install order in prod script.

--- runForProd.sh
+++ runForProd.sh
@@ -2,8 +2,8 @@
 source dev_build/buildFns.sh
 sudo chown -R vagrant:vagrant *
 cleanUpAllJs
+docker run -it --rm -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal; npm install;"
 linkNodeLib "lodash-es" "lodash-lib"
 compileAoT
 export ENV=production
-docker run -it --rm -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal; npm install;"
 docker-compose up
